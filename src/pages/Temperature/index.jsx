df8ef4c72dfe845a7f3e7090cb5a9091ee18ffb1
DaniloColares <dancolaresaquino@gmail.com>
2024-09-23 12:42:50 -0300

    mmmm

--- src/pages/Temperature/index.jsx
+++ src/pages/Temperature/index.jsx
@@ -1,18 +1,31 @@
 import { useNavigate } from "react-router-dom";
 import { Container, ArrowIcon } from "./styles";
-
+import mqtt from "mqtt"
 import { FiArrowLeft, FiSun } from "react-icons/fi";
+const TEMPERATURE_TOPIC = 'esp32/temperature'
 
 export function Temperature() {
-  const navigate = useNavigate();
+  const [metrics, setMetrics] = useState('') 
+    
+  useEffect(() => {
+    const client = mqtt.connect('ws://localhost:8080') //connecta ao mqtt broker
+    client.on('connect', handleConnection) //Ao conectar chama a função `handleConnection`
 
+    function handleConnection () {
+      client.subscribe(TEMPERATURE_TOPIC) //Escuta o tópico de temperatura
+
+      client.on('message', (topic, message) => { //Ao recebem mensagem, executa essa função que recebe a mensagem que e seu topico
+        if(topic === TEMPERATURE_TOPIC) setMetrics(message.toString())
+      })
+    }
+  }, [])
   return(
     <Container>
       <ArrowIcon onClick={() => navigate("/home")}>
         <FiArrowLeft size={50}/>
       </ArrowIcon>
       <FiSun size={350}/>
-      <p>A temperatura atual no jarro é: </p>
+      <p>A temperatura atual no jarro é:{metrics} </p>
     </Container>
   )
 }
